d5526597f1e79a4d5c441fd99617d8761c1e3acf
F3lixitas <felixriedel25@gmail.com>
2021-07-03 23:09:25 +0200

    nettoyage et explication

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Un jeu adapté du jeux de société éponyme
 
 Lien vers la documentation Godot : https://docs.godotengine.org/en/stable/classes/class_%40gdscript.html
+
+Génération du plateau :
+- génération des noeuds et de leur position.
+- génération des triangles du plateau en 4 étapes : en bas à gauche (0), en hau à droite (1), exception de la ligne du haut et exception de la ligne du bas
+En itérant sur X + 1 on peut faire l'exeception des bords droite et gauche dans la foulée.
+Un triangle est partagé par 3 noeuds (sauf exception des bords).
+Le triangle en bas à gauche peut être en position 0, 2 et 4. Le triangle en haut à gauche peut être en position 1, 3 et 5.
--- scripts/nodeGenerator_script.gd
+++ scripts/nodeGenerator_script.gd
@@ -1,14 +1,121 @@
 extends Node2D
 
-var triangles = Array()
-var nodes = Array()
-var maxX = 24
-var maxY = 13
+var nodes = Array()            #tableau des noeuds de notre plateau
+var maxX = 24                  #nb de noeuds en X
+var maxY = 13                  #nb de noeuds en Y
+
+var stepX = 64                 #modifier pour changer la taille en pixels du plateau, mettre des valeurs paires
+var stepY = int(stepX * 0.866) #ne pas modifier, 0.866 est le ratio pour un triangle equilateral
+
+var padding = 100              #définit l'espace entre le plateau et le bord
+var lineWidth = 4              #définit la largeur des lignes entre les triangles, mettre des valeurs paires
 
 var Noeud = {
-	"tri": Array(),
-	"pos": Vector2()
+	"tri": Array(),            #tableau des indices des triangles, permet de faciliter le changement de couleur
+	"pos": Vector2()           #position du noeud
 }
+
+#fonctions de génération
+#remplit le tableau de noeuds avec la bonne position
+func genererNoeuds():
+	for i in range(0, maxX * maxY):
+		var p = Noeud.duplicate()
+		p.pos.x = (i % maxX) * stepX + ((int(i / maxX) % 2) * int(stepX / 2)) + padding
+		p.pos.y = int(i / maxX) * stepY + padding
+		p.tri = [0, 0, 0, 0, 0, 0]
+		nodes.append(p)
+
+func genererTriangles():
+	for i in range(0, (maxX + 1) * maxY):
+		var st = SurfaceTool.new()
+		st.begin(Mesh.PRIMITIVE_TRIANGLES)
+		var tempM = Mesh.new()
+		var px = (i % (maxX + 1)) * stepX + ((int(i / (maxX + 1)) % 2) * int(stepX / 2)) + (padding - stepX)
+		var py = int(i / (maxX + 1)) * stepY + padding
+		st.add_vertex(Vector3(px + lineWidth, py + int(lineWidth / 2), 0))
+		st.add_vertex(Vector3(px + stepX - lineWidth, py + int(lineWidth / 2), 0))
+		st.add_vertex(Vector3(px + int(stepX / 2), py + stepY - lineWidth, 0))
+		st.commit(tempM)
+		var meshI = MeshInstance2D.new()
+		meshI.mesh = tempM
+		add_child(meshI)
+		
+		var x = i % (maxX + 1)
+		var y = int(i / (maxX + 1))
+		
+		if not (x == maxX):
+			nodes[i - y].tri[0] = i
+		if not (x == 0):
+			nodes[i - y - 1].tri[4] = i
+		if (not (x == 0)) and (y % 2 == 0) and (not (y == maxY - 1)):
+			nodes[i - y - 1 + maxX].tri[2] = i
+		elif (not (x == maxX)) and (y % 2 == 1) and (not (y == maxY - 1)):
+			nodes[i - y + maxX].tri[2] = i
+	
+	for i in range(0, (maxX + 1) * maxY):
+		var st = SurfaceTool.new()
+		st.begin(Mesh.PRIMITIVE_TRIANGLES)
+		var tempM = Mesh.new()
+		var px = (i % (maxX + 1)) * 64 + ((int(i / (maxX + 1)) % 2) * 32) + 36
+		var py = int(i / (maxX + 1)) * 55 + 100
+		st.add_vertex(Vector3(px + 4, py - 2, 0))
+		st.add_vertex(Vector3(px + 60, py - 2, 0))
+		st.add_vertex(Vector3(px + 32, py - 51, 0))
+		st.commit(tempM)
+		var meshI = MeshInstance2D.new()
+		meshI.mesh = tempM
+		add_child(meshI)
+		
+		var x = i % (maxX + 1)
+		var y = int(i / (maxX + 1))
+		
+		var index = i + (maxX + 1) * maxY
+		
+		if not (x == maxX):
+			nodes[i - y].tri[1] = index
+		if not (x == 0):
+			nodes[i - y - 1].tri[3] = index
+		if (not (x == 0)) and (y % 2 == 0) and (not (y == 0)):
+			nodes[i - y - 1 - maxX].tri[5] = index
+		elif (not (x == maxX)) and (y % 2 == 1) and (not (y == 0)):
+			nodes[i - y - maxX].tri[5] = index
+	
+	for i in range(0, maxX):
+		var st = SurfaceTool.new()
+		st.begin(Mesh.PRIMITIVE_TRIANGLES)
+		var tempM = Mesh.new()
+		var px = (i % maxX) * 64 + 100
+		var py = 100
+		st.add_vertex(Vector3(px, py - 4, 0))
+		st.add_vertex(Vector3(px - 27, py - 51, 0))
+		st.add_vertex(Vector3(px + 27, py - 51, 0))
+		st.commit(tempM)
+		var meshI = MeshInstance2D.new()
+		meshI.mesh = tempM
+		add_child(meshI)
+		
+		var index = i + (maxX + 1) * maxY * 2
+		nodes[i].tri[2] = index
+	
+	for i in range(0, maxX):
+		var st = SurfaceTool.new()
+		st.begin(Mesh.PRIMITIVE_TRIANGLES)
+		var tempM = Mesh.new()
+		var px = (i % maxX) * 64 + 100 + (((maxY + 1) % 2) * 32)
+		var py = 100 + (maxY - 1) * 55
+		st.add_vertex(Vector3(px, py + 4, 0))
+		st.add_vertex(Vector3(px - 27, py + 51, 0))
+		st.add_vertex(Vector3(px + 27, py + 51, 0))
+		st.commit(tempM)
+		var meshI = MeshInstance2D.new()
+		meshI.mesh = tempM
+		add_child(meshI)
+		
+		var index = i + (maxX + 1) * maxY * 2 + maxX
+		nodes[maxX * (maxY - 1) + i].tri[5] = index
+
+
+
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT:
@@ -39,101 +146,6 @@ func _unhandled_input(event):
 			get_child(nodes[i].tri[5]).visible = true
 
 func _init():
-	for i in range(0, maxX * maxY):
-		var p = Noeud.duplicate()
-		p.pos.x = (i % maxX) * 64 + ((int(i / maxX) % 2) * 32) + 100
-		p.pos.y = int(i / maxX) * 55 + 100
-		p.tri = [0, 0, 0, 0, 0, 0]
-		nodes.append(p)
+	genererNoeuds()
+	genererTriangles()
 	
-	for i in range(0, (maxX + 1) * maxY):
-		var st = SurfaceTool.new()
-		st.begin(Mesh.PRIMITIVE_TRIANGLES)
-		var tempM = Mesh.new()
-		var px = (i % (maxX + 1)) * 64 + ((int(i / (maxX + 1)) % 2) * 32) + 36
-		var py = int(i / (maxX + 1)) * 55 + 100
-		st.add_vertex(Vector3(px + 5, py + 2, 0))
-		st.add_vertex(Vector3(px + 59, py + 2, 0))
-		st.add_vertex(Vector3(px + 32, py + 53, 0))
-		st.commit(tempM)
-		var meshI = MeshInstance2D.new()
-		meshI.mesh = tempM
-		triangles.append(meshI)
-		add_child(meshI)
-		
-		var x = i % (maxX + 1)
-		var y = int(i / (maxX + 1))
-		
-		if not (x == maxX):
-			nodes[i - y].tri[0] = i
-		if not (x == 0):
-			nodes[i - y - 1].tri[4] = i
-		if (not (x == 0)) and (y % 2 == 0) and (not (y == maxY - 1)):
-			nodes[i - y - 1 + maxX].tri[2] = i
-		elif (not (x == maxX)) and (y % 2 == 1) and (not (y == maxY - 1)):
-			nodes[i - y + maxX].tri[2] = i
-	
-	for i in range(0, (maxX + 1) * maxY):
-		var st = SurfaceTool.new()
-		st.begin(Mesh.PRIMITIVE_TRIANGLES)
-		var tempM = Mesh.new()
-		var px = (i % (maxX + 1)) * 64 + ((int(i / (maxX + 1)) % 2) * 32) + 36
-		var py = int(i / (maxX + 1)) * 55 + 100
-		st.add_vertex(Vector3(px + 5, py - 2, 0))
-		st.add_vertex(Vector3(px + 59, py - 2, 0))
-		st.add_vertex(Vector3(px + 32, py - 53, 0))
-		st.commit(tempM)
-		var meshI = MeshInstance2D.new()
-		meshI.mesh = tempM
-		triangles.append(meshI)
-		add_child(meshI)
-		
-		var x = i % (maxX + 1)
-		var y = int(i / (maxX + 1))
-		
-		var index = i + (maxX + 1) * maxY
-		
-		if not (x == maxX):
-			nodes[i - y].tri[1] = index
-		if not (x == 0):
-			nodes[i - y - 1].tri[3] = index
-		if (not (x == 0)) and (y % 2 == 0) and (not (y == 0)):
-			nodes[i - y - 1 - maxX].tri[5] = index
-		elif (not (x == maxX)) and (y % 2 == 1) and (not (y == 0)):
-			nodes[i - y - maxX].tri[5] = index
-	
-	for i in range(0, maxX):
-		var st = SurfaceTool.new()
-		st.begin(Mesh.PRIMITIVE_TRIANGLES)
-		var tempM = Mesh.new()
-		var px = (i % maxX) * 64 + 100
-		var py = 100
-		st.add_vertex(Vector3(px, py - 2, 0))
-		st.add_vertex(Vector3(px - 27, py - 53, 0))
-		st.add_vertex(Vector3(px + 27, py - 53, 0))
-		st.commit(tempM)
-		var meshI = MeshInstance2D.new()
-		meshI.mesh = tempM
-		triangles.append(meshI)
-		add_child(meshI)
-		
-		var index = i + (maxX + 1) * maxY * 2
-		nodes[i].tri[2] = index
-	
-	for i in range(0, maxX):
-		var st = SurfaceTool.new()
-		st.begin(Mesh.PRIMITIVE_TRIANGLES)
-		var tempM = Mesh.new()
-		var px = (i % maxX) * 64 + 100 + (((maxY + 1) % 2) * 32)
-		var py = 100 + (maxY - 1) * 55
-		st.add_vertex(Vector3(px, py + 2, 0))
-		st.add_vertex(Vector3(px - 27, py + 53, 0))
-		st.add_vertex(Vector3(px + 27, py + 53, 0))
-		st.commit(tempM)
-		var meshI = MeshInstance2D.new()
-		meshI.mesh = tempM
-		triangles.append(meshI)
-		add_child(meshI)
-		
-		var index = i + (maxX + 1) * maxY * 2 + maxX
-		nodes[maxX * (maxY - 1) + i].tri[5] = index
