281b565209653950c4ba377ca76be17ea12b98d3
F3lixitas <felixriedel25@gmail.com>
2021-07-16 20:17:36 +0200

    nettoyage du code
    Délégation de la fonction de génération des triangles dans le fichier triangleGenerator_script.gd
    Création du fichier pointOperations_script.gd que Landry devra modifier
    Nettoyage des commentaires

---
+++ scripts/pointOperations_script.gd
@@ -0,0 +1,13 @@
+extends Node
+
+#######################   Note à Landry   #######################
+# Créé-moi ici les fonctions qui me permettent d'obtenir        #
+# l'indice du point selon la position du curseur et d'obtenir   #
+# la position du point grace à son indice.                      #
+#################################################################
+
+func getPointIndex(maxX, maxY, stepX, stepY, padding, cursorPosition):
+	return 0
+
+func getPointPosition(maxX, maxY, stepX, stepY, padding, index):
+	return 0
