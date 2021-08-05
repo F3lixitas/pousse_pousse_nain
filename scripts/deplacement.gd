107523e1dc9a7c32d7fb7de926171fffc42e419c
Landry-B <landry.bailly@hotmail.fr>
2021-08-05 19:38:14 +0200

    test

---
+++ scripts/deplacement.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+static func deplacement(posInit, dep1, dep2, dep3, dep4, dep5, dep6, tabData):
+	##### Variable #######
+	
+	
+	##### initialisation ######
+	
+	
+	#### Programme ######
+	
+	
+	return true; 
