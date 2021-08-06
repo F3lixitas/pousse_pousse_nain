25e579eea014a9b791c9468d55b6b572494b1ce9
Landry-B <landry.bailly@hotmail.fr>
2021-08-07 01:12:23 +0200

    Debug Final

--- scripts/deplacement.gd
+++ scripts/deplacement.gd
@@ -1,3 +1,9 @@
+# ATTENTION : Cette version marche parfaitement mais elle est à chier en terme d'optimisation
+#
+#Car on parcours même les chemins nons valide. Voir fichier sauv fct dep avec beug mais economie.txt pour éviter de faire ça
+# Si on fait pas ça, on peut avoir des beugs. Es ce possible de trouver un compromis ?
+
+
 extends Node
 
 const pointOperations = preload("res://scripts/pointOperations_script.gd")
@@ -6,7 +12,10 @@ static func verfifAllDep(posInit, dep, node):
 	##### Variable #######
 	var Chemin = {
 		"dir": [0, 0, 0, 0, 0, 0],             
-		"rang" : int(),      
+		"rang" : int(),
+		"valide" : int(), # 1 pour valide, 0 pour non valide 
+				#J'ai rajouter cette option car les chemins nons valide doivent continuer d'être tester. Car si ils tombent sur une case occupé, ils doivent avertir leur voisin !
+				# Tester les chemins non valides rajoute bcp de tests inutile, mais j'ai pas trouvé mieux.
 	}
 	var cheminsBase = Array()
 	var cheminsCourant = Array()
@@ -18,16 +27,17 @@ static func verfifAllDep(posInit, dep, node):
 	for i in range(0, 6):
 		var monChemin = Chemin.duplicate(true)
 		monChemin.dir[i] = 1
+		monChemin.rang = 1
 		if dep[i] == 1 and pointOperations.checkValidity(node.points[posInit].neighbour[i], node):
 			#Pour les chemins de bases, il faudra aussi vérifier les colisions !!! Utilise si quelqu'un nous touche deirectement
-			monChemin.rang = 1
+			monChemin.valide = 1
 			indicesOK.append(node.points[posInit].neighbour[i])
 		else:
-			monChemin.rang = 0
+			monChemin.valide = 0
 		cheminsCourant.push_back(monChemin)
 		
 	#### Programme (a partir de l'étage 2) ######
-	for etageCourant in range(2, 10):
+	for etageCourant in range(2, 20):
 		
 		#print("ETAGE n", etageCourant)
 		
@@ -45,6 +55,7 @@ static func verfifAllDep(posInit, dep, node):
 			if cheminsBase[j].rang + cheminsBase[j + 1].rang == etageCourant:
 				var monChemin = Chemin.duplicate(true)
 				monChemin.rang = etageCourant
+				monChemin.valide = cheminsBase[j].valide*cheminsBase[j+1].valide
 				for dirCourant in range(0, 6):
 					monChemin.dir[dirCourant] = cheminsBase[j].dir[dirCourant] + cheminsBase[j + 1].dir[dirCourant]
 				cheminsCourant.push_back(monChemin.duplicate(true))
@@ -54,6 +65,7 @@ static func verfifAllDep(posInit, dep, node):
 		if cheminsBase[cheminsBase.size()-1].rang + cheminsBase[0].rang == etageCourant:
 			var monChemin = Chemin.duplicate(true)
 			monChemin.rang = etageCourant
+			monChemin.valide = cheminsBase[cheminsBase.size()-1].valide*cheminsBase[0].valide
 			for dirCourant in range(0, 6):
 				monChemin.dir[dirCourant] = cheminsBase[cheminsBase.size()-1].dir[dirCourant] + cheminsBase[0].dir[dirCourant]
 			cheminsCourant.push_back(monChemin.duplicate(true))
@@ -62,9 +74,7 @@ static func verfifAllDep(posInit, dep, node):
 		
 		### Vérification des cases à l'étage courant pour chaque chemin ###
 		for j in range(0, cheminsCourant.size()):
-			if cheminsCourant[j].rang != 0 and etageCourant % cheminsCourant[j].rang == 0:
-				#print(cheminsCourant[j])
-				
+			if etageCourant % cheminsCourant[j].rang == 0:
 				#On récup l'indice de la case en passant par tous les voisins depuis le point de départ
 				var indiceCourant = posInit
 				for dirCourant in range(0, 6):
@@ -72,11 +82,12 @@ static func verfifAllDep(posInit, dep, node):
 						if indiceCourant >= 0 :
 							indiceCourant = node.points[indiceCourant].neighbour[dirCourant]
 				
+				#Si la case est occupé, alors on dévalide la case et les alentours
 				if pointOperations.checkValidity(indiceCourant, node) == false:
 					# la case est occupé, donc on suppr le chemin et tous les chemins autours qui n'ont pas de point à cet étage
 					
 					#Chemin courant
-					cheminsCourant[j].rang = 0
+					cheminsCourant[j].valide = 0
 					
 					#Chemin avant
 					var indiceTest = j
@@ -87,10 +98,10 @@ static func verfifAllDep(posInit, dep, node):
 						else:
 							indiceTest = cheminsCourant.size() - 1
 						
-						if cheminsCourant[indiceTest].rang == 0 or etageCourant % cheminsCourant[indiceTest].rang == 0:
+						if etageCourant % cheminsCourant[indiceTest].rang == 0:
 							break
 						else:
-							cheminsCourant[indiceTest].rang = 0
+							cheminsCourant[indiceTest].valide = 0
 							
 					#Chemin après
 					indiceTest = j
@@ -101,12 +112,12 @@ static func verfifAllDep(posInit, dep, node):
 						else:
 							indiceTest = 0
 						
-						if cheminsCourant[indiceTest].rang == 0 or etageCourant % cheminsCourant[indiceTest].rang == 0:
+						if etageCourant % cheminsCourant[indiceTest].rang == 0:
 							break
 						else:
-							cheminsCourant[indiceTest].rang = 0
+							cheminsCourant[indiceTest].valide = 0
 							
-				else:
+				else: if cheminsCourant[j].valide == 1:
 					indicesOK.append(indiceCourant)
 				#print("indice : ", indiceCourant)		
 				
